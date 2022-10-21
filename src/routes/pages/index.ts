94ca4945ef49dec1b48877e601862360808b4b79
Daan Verstraten <daan@q42.nl>
2022-10-21 15:43:31 +0200

    Updated

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/simple", (req, res) => {
-  res.sendFile(__dirname, "simple.html");
+  res.sendFile(__dirname, "..", "..", "..", "simple.html");
 });
 
 module.exports = router;
