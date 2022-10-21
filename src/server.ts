fbad2311e2f27fac6d88f7a8f6c2317848bed5fa
Daan Verstraten <daan@q42.nl>
2022-10-21 09:43:07 +0200

    Adding code

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+export function setupServer() {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+
+    console.log(req);
+  });
+
+  return server;
+}
