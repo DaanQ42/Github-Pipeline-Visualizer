d173eeaa1e1ea2ba675d092e82ea163edf837085
Daan Verstraten <daan@q42.nl>
2022-10-21 15:45:46 +0200

    Updated

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import path from "path";
 const router = express.Router();
 
+const pagesFolder = path.join(__dirname, "..", "..", "..", "pages");
+
 router.get("/simple", (req, res) => {
-  res.sendFile(__dirname, "..", "..", "..", "simple.html");
+  res.sendFile(pagesFolder, "simple.html");
 });
 
 module.exports = router;
