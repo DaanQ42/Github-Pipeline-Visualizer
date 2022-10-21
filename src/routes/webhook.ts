bc736b37b6f3c44881b9ed68482d8f8f84fe6983
Daan Verstraten <daan@q42.nl>
2022-10-21 13:42:44 +0200

    Adding middleware

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,9 +1,21 @@
 import express from "express";
 const router = express.Router();
 
+router.use((req, res, next) => {
+  const sign = req.headers["X-Hub-Signature-256"];
+
+  console.log(`sign: ${sign}`);
+  next();
+});
+
 router.get("/", (req, res) => {
   console.log(req);
   res.send("ping");
+});
+
+router.post("/", (req, res) => {
+  console.log(req);
+  res.send("pong");
 });
 
 module.exports = router;
