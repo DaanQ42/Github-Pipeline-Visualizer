648ece095a66200466a00bfa3448b95fab3d562c
Daan Verstraten <daan@q42.nl>
2022-10-21 13:46:05 +0200

    Getting sign

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.use((req, res, next) => {
-  const sign = req.headers["X-Hub-Signature-256"];
+  const sign = req.get("X-Hub-Signature-256");
 
   console.log(`sign: ${sign}`);
   next();
