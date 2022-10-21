7bed7ab793ba212465617095f848c014efd36166
Daan Verstraten <daan@q42.nl>
2022-10-21 15:52:33 +0200

    Added

--- src/routes/pages/index.ts
+++ src/routes/pages/index.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 const pagesFolder = path.join(__dirname, "..", "..", "..", "pages");
 
 router.get("/simple", (req, res) => {
+  console.log(`simple.html ${pagesFolder}`);
   res.sendFile(pagesFolder, "simple.html");
 });
 
