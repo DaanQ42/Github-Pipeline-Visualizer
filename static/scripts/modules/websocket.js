237546c815b9c7628baf2439a9f85982e20b74e0
Daan Verstraten <daan@q42.nl>
2022-12-16 10:55:39 +0100

    Adding first test

--- static/scripts/modules/websocket.js
+++ static/scripts/modules/websocket.js
@@ -1,9 +1,16 @@
 export class EventConnection {
+  ws;
+
   /**
    *
    * @param {string} url
    */
   constructor(url) {
+    console.log(`Connecting to ${url}`);
     this.ws = new WebSocket(url);
+  }
+
+  on(event, callback) {
+    this.ws.on(event, callback);
   }
 }
